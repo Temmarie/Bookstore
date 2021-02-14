80b7453bd85da16d1a7daab274653036cfa75c06
Grace Tamara Johnson E <49621279+Temmarie@users.noreply.github.com>
2021-02-14 21:48:16 +0100

    Connect removeBook action and add new table column to Bookslist

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,16 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line arrow-body-style
-const Book = ({ book }) => {
+const Book = ({ book, handleRemoveBook }) => {
   const {
     author, id, title, category,
   } = book;
+
   return (
     <tr>
       <td>{author}</td>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td>
+        <button type="button" onClick={() => handleRemoveBook(book)}>
+          Remove Book
+        </button>
+      </td>
     </tr>
   );
 };
@@ -23,6 +29,7 @@ Book.propTypes = {
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
+  handleRemoveBook: PropTypes.func.isRequired,
 };
 
 export default Book;
